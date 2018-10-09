1961172dcedbfd6eee0beedde26940b1987bc7cc
Jason Taylor <jason@jasongtaylor.com>
2018-10-08 22:00:05 -0400

    Update secure-jenkins.sh

--- aws/lightsail/secure-jenkins.sh
+++ aws/lightsail/secure-jenkins.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+systemctl stop nginx
+
+cd /etc/nginx/conf.d
+if [ -f lightsail-jenkins.conf ]; then
+  rm lightsail-jenkins.conf
+fi
+
+cd /etc/nginx/sites-available
+wget https://raw.githubusercontent.com/jenkins-training/jenkins-bootcamp-course/master/aws/lightsail/jenkins-secured.conf
+
