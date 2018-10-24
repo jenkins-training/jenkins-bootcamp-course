18529214e4c37288936a14bcf53e55d29b8ea056
Jason Taylor <jason@jasongtaylor.com>
2018-10-24 01:01:26 -0400

    Update user account stuff

--- aws/lightsail/scale/jenkins-build-server.sh
+++ aws/lightsail/scale/jenkins-build-server.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Update from OS install
-apt update -y
+apt-get update -y
 
 # Common utilities
-apt install -y nano zip unzip wget curl git
+apt-get install -y nano zip unzip wget curl git build-essentials
 
 # Install Java 8
-apt install -y openjdk-8-jdk openjdk-8-jdk-headless
+apt-get install -y openjdk-8-jdk openjdk-8-jdk-headless
 
 # Docker (official)
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-cd/etc/apt/sources.list.d
+cd /etc/apt/sources.list.d
 cat "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" >> docker.list
-apt update -y
-apt install -y docker-ce
+apt-get update -y
+apt-get install -y docker-ce
 docker --version
 
 cd /usr/local
@@ -24,8 +24,9 @@ cd /usr/local
 # Setup SDK Man
 export SDKMAN_DIR="/usr/local/sdkman"
 curl -s "https://get.sdkman.io" | bash
-cat "sdkman_auto_answer=true" >> $SDKMAN_DIR/etc/config
-source $SDKMAN_DIR/bin/sdkman-init.sh
+sleep 10
+cat "sdkman_auto_answer=true" >> "$SDKMAN_DIR/etc/config"
+source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
 sdk version
 sdk install groovy
@@ -49,16 +50,20 @@ ln -s sdkman/candidates/kotlin kotlin
 ln -s sdkman/candidates/kscript kscript
 
 # Node via NVM
+cd /usr/local
+mkdir -p /usr/local/nvm
+chmod 755 nvm
 export NVM_DIR="/usr/local/nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+sleep 10
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
 nvm install --lts
 
 # Setup Jenkins user
 cd
-useradd jenkins
-usermod -aG sudo jenkins
+adduser --disabled-password --gecos "" jenkins
+adduser jenkins sudo
 cd /home/jenkins
 mkdir .ssh
 chmod 700 .ssh
