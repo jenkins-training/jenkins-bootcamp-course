57344e4f2d505dcf0cc7ed29a0800c76ff8a5664
Jason Taylor <jason@jasongtaylor.com>
2018-10-25 00:16:17 -0400

    Add Scala, Kotlin and Go

--- aws/lightsail/scale/jenkins-build-server.sh
+++ aws/lightsail/scale/jenkins-build-server.sh
@@ -7,13 +7,16 @@ GRADLE_VERSION="4.10.2"
 GROOVY_VERION="2.5.3"
 SCALA_VERION="2.12.7"
 SBT_VERSION="1.2.6"
+KOTLIN_VERSION="1.2.71"
+GO_VERSION="1.11.1"
+
 
 # Update from OS install
 apt-get update -y
 sleep 10
 
 # Common utilities
-apt-get install -y nano wget curl git
+apt-get install -y nano wget curl git python3
 apt-get install -y zip unzip
 apt-get install -y build-essentials
 
@@ -29,6 +32,12 @@ echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" >
 apt-get update -y
 apt-get install -y docker-ce
 docker --version
+
+# Python 3, Pip and AWS CLI
+apt-get install -y python3 python3-pip
+python3 --version
+pip3 --version
+pip install awscli
 
 # Install stuff in /usr/local
 cd /usr/local
@@ -66,14 +75,38 @@ ln -s apache-groovy-$GROOVY_VERION groovy
 ln -s groovy/bin/groovy bin/groovy
 
 # SBT
-https://piccolo.link/sbt-$SBT_VERSION.tgz
+wget https://piccolo.link/sbt-$SBT_VERSION.tgz
 tar -xvzf sbt-$SBT_VERSION.tgz
 rm sbt-$SBT_VERSION.tgz
 chmod 755 sbt-$SBT_VERSION
 ln -s sbt-$SBT_VERSION sbt
 ln -s sbt/bin/sbt bin/sbt
 
+# Scala
+wget https://downloads.lightbend.com/scala/$SCALA_VERION/scala-$SCALA_VERION.tgz
+tar -xvzf scala-$SCALA_VERION.tgz
+rm scala-$SCALA_VERION.tgz
+chmod 755 scala-$SCALA_VERION
+ln -s scala-$SCALA_VERION scala
+ln -s scala/bin/scala bin/scala
 
+# Kotlin
+wget https://github.com/JetBrains/kotlin/releases/download/v$KOTLIN_VERSION/kotlin-compiler-$KOTLIN_VERSION-linux-x64.zip
+unzip kotlin-compiler-$KOTLIN_VERSION-linux-x64.zip
+rm kotlin-compiler-$KOTLIN_VERSION-linux-x64.zip
+mv kotlinc kotlin-$KOTLIN_VERSION
+ln -s kotlin-$KOTLIN_VERSION kotlin
+ln -s kotlin/bin/kotlin bin/kotlin
+ln -s kotlin/bin/kotlinc bin/kotlinc
+ln -s kotlin/bin/kotlin-compiler bin/kotlin-compiler
+
+# Go Lang
+wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
+tar -xvzf go$GO_VERSION.linux-amd64.tar.gz
+rm go$GO_VERSION.linux-amd64.tar.gz
+mv go go-$GO_VERSION
+ln -s go-$GO_VERSION go
+ln -s go/bin/go bin/go
 
 # Node via NVM
 cd /usr/local
