965bd2c66f91cd9cdc1bd7a48f518e4a00ce4981
Jason Taylor <jason@jasongtaylor.com>
2022-04-04 03:11:32 -0400

    Local VM server files for simple Jenkins server

---
+++ local/simple/ubuntu/README.md
@@ -0,0 +1,21 @@
+# Jenkins Installation
+
+## Purpose
+
+This guide will help setup a Jenkins server quickly on Ubuntu Server 20.04 LTS.
+
+## Requirements
+
+* Ubuntu 20.04 LTS (tested with server) - x86/amd64 or Arm64
+* Networking working
+
+## Included
+
+The following items will be installed/setup:
+
+* Basic tools like zip, nano, git
+* OpenJDK 11 (headless)
+* Jenkins (server)
+* Nginx
+    * Add reverse proxy config to point to Jenkins server
+
---
+++ local/simple/ubuntu/jenkins-server-only.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+### VARS
+
+# Maven Version
+MVN_VER=3.8.5
+
+### END VARS
+
+echo "Jenkins Server install script for Ubuntu"
+echo "Script has been prepared for and tested on Ubuntu 20.04.x LTS"
+
+# Update from OS install
+apt-get update -y
+
+# Common utilities
+apt-get install -y nano zip unzip wget curl git
+
+# Install Java 11
+apt-get install -y openjdk-11-jre-headless openjdk-11-jdk-headless
+
+## Maven Install
+cd /usr/local
+wget http://www-us.apache.org/dist/maven/maven-3/$MVN_VER/binaries/apache-maven-$MVN_VER-bin.tar.gz
+tar -xvzf apache-maven-$MVN_VER-bin.tar.gz
+ln -s apache-maven-$MVN_VER maven
+chown -R root.root apache-maven-$MVN_VER
+chmod 755 apache-maven-$MVN_VER
+ln -s maven/bin/mvn bin/mvn
+
+# install Jenkins
+cd
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+cd /etc/apt/sources.list.d
+echo "deb https://pkg.jenkins.io/debian-stable binary/" >> jenkins.list
+apt-get update -y
+apt-get install -y jenkins
+
+systemctl start jenkins
+systemctl enable jenkins
+
+
+echo "Installing Nginx to serve as reverse proxy"
+
+apt-get install -y nginx
+
+# remove default symlink
+cd /etc/nginx/sites-enabled
+rm default
+
+cd /etc/nginx/conf.d
+
+cat << EOF > jenkins-proxy.conf
+server {
+  listen 80;
+  listen [::]:80;
+  
+  location / {
+      proxy_set_header Host $host;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_pass http://localhost:8080/;
+  }
+}
+EOF
+
+systemctl restart nginx
+systemctl enable nginx
+
+echo "If running on Ubuntu Desktop - access http://localhost/"
+echo "Otherwise: Update your hosts file to point to this IP address"
+
+cd
